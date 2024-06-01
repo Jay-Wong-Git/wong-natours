3009cf9e4793b9fc8b61ee02d1431df5c85c1721
= <=>
2024-06-01 16:47:41 +0800

    feat: Separate errors in production env and development env

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,5 +1,32 @@
+const sendErrorDev = (err, res) =>
+  res.status(err.statusCode).json({
+    status: err.status,
+    msg: err.message,
+    error: err,
+    stack: err.stack,
+  });
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({ status: err.status, msg: err.message });
+    // Programming, unknown error: don't leak error details
+  } else {
+    // 1) Log the error
+    console.error('ERROR 💥', err);
+    // 2) Send generic message
+    res
+      .status(500)
+      .json({ status: 'error', msg: 'Something went very wrong!' });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({ status: err.status, msg: err.message });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
