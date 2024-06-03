84fcbab349f4ac7ac57fc3b1e02ba3dc3654e347
= <=>
2024-06-03 10:51:28 +0800

    feat: implement rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,19 +10,34 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
 
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+// LOGGER
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
+// RATE LIMITER
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in ONE HOUR!',
+});
+app.use('/api', limiter);
+
+// PARSE JSON
 app.use(express.json()); // access request body
 
+// STATIC FILES
 app.use(express.static(`${__dirname}/public`)); // static files
 
+// DIY CONSOLE MESSAGE
 // app.use((req, res, next) => {
 //   console.log('Hello from the middleware 👋');
 //   next();
 // });
 
+// DIY REQUEST PROPERTY
 app.use((req, res, next) => {
   req.requestTime = `⏰ ${new Date().toLocaleString()}`;
   next();
@@ -30,11 +47,8 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// CONFIG 404
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   msg: `Can't find '${req.originalUrl}' on the server`,
-  // });
   const err = new AppError(
     `Can't find '${req.originalUrl}' on the server`,
     404,
