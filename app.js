e32317cad3f4921cfc70b025735fbc352b5519af
= <=>
2024-06-03 11:25:26 +0800

    feat: implement data sanitization against no-sql injection and xss

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,6 +33,12 @@ app.use('/api', limiter);
 
 // BODY PARSER, READING DATE FROM body INTO req.body
 app.use(express.json({ limit: '10kb' })); // access request body
+
+// DATA SANITIZATION AGAINST NO-SQL QUERY INJECTION
+app.use(mongoSanitize()); // {"email": {"$gt": ""}, "password": "your-pass"}
+
+// DATA SANITIZATION AGAINST XSS
+app.use(xss());
 
 // SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`)); // static files
