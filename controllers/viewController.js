f923e82a5fa77e50aa05aa61eeba226762df2fd3
= <=>
2024-06-05 14:29:37 +0800

    feat: build overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,14 @@
-exports.getOverview = (req, res) =>
-  res.status(200).render('overview', { title: 'All Tours' });
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build template
+  // 3) Render the template using tour date from 1)
+  return res.status(200).render('overview', { title: 'All Tours', tours });
+});
 
 exports.getTour = (req, res) =>
   res.status(200).render('tour', { title: 'The Forest Hiker' });
